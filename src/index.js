1e7e070331e914585ada3d6a7403225dc5f1c919
dhafin <dhafinrazaq@gmail.com>
2020-08-12 13:44:20 +0700

    improved drag UX

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@
 import Sortable from "sortablejs";
 
 var el = document.getElementById("items");
-var sortable = Sortable.create(el);
+var sortable = Sortable.create(el, {
+  animation: 150,
+  ghostClass: "sortable-ghost",
+});
+
+// const codes = []
+
+// function submitNewCode(e) {
+//     e.preventDefault();
+//     const title = e.target.title.value;
+//     renderProjectList();
+//   }
